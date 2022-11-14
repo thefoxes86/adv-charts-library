076c07b9f3f044f1d434aaf080288c078a737ab1
thefoxes86 <nicola.volpi86@gmail.com>
2022-11-15 00:47:26 +0100

    Automate publish process with versioning

--- src/Generatechartsadv.js
+++ src/Generatechartsadv.js
@@ -1,5 +1,4 @@
 "use strict";
-// Version 1.0.4
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
